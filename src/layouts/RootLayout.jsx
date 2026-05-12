1f20634742f05c9c92dba81950fbba8fd1462b61
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-05-12 14:29:29 +0600

    added basic theme,updated footer background

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -4,7 +4,7 @@ import Navbar from "../pages/shared/Navbar/Navbar";
 
 const RootLayout = () => {
     return (
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-7xl mx-auto ">
             <Navbar></Navbar>
             <Outlet></Outlet>  
             <Footer></Footer>          
