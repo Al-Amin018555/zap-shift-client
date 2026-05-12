ab56fce526b426d41299a5e130b6930ddf48fe1d
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-05-12 13:19:59 +0600

    added logo in nav & footer afterupdating it,width set for root layout

--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -1,3 +1,5 @@
+import Logo from "../../../components/logo/Logo";
+
 const Navbar = () => {
     return (
         <div className="navbar bg-base-100 shadow-sm">
@@ -20,7 +22,9 @@ const Navbar = () => {
                         <li><a>Item 3</a></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">
+                    <Logo></Logo>
+                </a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
