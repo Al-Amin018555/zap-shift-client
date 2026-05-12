1d34f741def078a126fd5e005c981d0d1c251a94
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-05-12 12:27:05 +0600

    start project with some dependency installment

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router/dom";
+import { router } from "./routes/router";
+import './index.css';
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />,
+);
