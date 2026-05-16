b032cea4f7fc20157f64e31a52932fb69b81c45d
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-05-16 19:55:09 +0600

    added review section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,7 +1,10 @@
 import Banner from '../Banner/Banner'
 import Brands from '../Brands/Brands';
 import HowItWorks from '../HowItWorks/HowItWorks'
+import Reviews from '../Reviews/Reviews';
 import Services from '../Services/Services'
+
+const reviewPromise = fetch("reviews.json").then(res => res.json());
 
 const Home = () => {
     return (
@@ -11,6 +14,7 @@ const Home = () => {
                 <HowItWorks></HowItWorks>
                 <Services></Services>
                 <Brands></Brands>
+                <Reviews reviewPromise={reviewPromise}></Reviews>
             </div>
         </div>
     );
---
+++ src/pages/Home/Reviews/Reviews.jsx
@@ -0,0 +1,50 @@
+import { use } from "react";
+import { Autoplay, EffectCoverflow, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import ReviewCard from "./ReviewCard";
+
+const Reviews = ({ reviewPromise }) => {
+    const reviews = use(reviewPromise);
+    return (
+        <div className="my-24">
+            <div className="text-center space-y-6">
+                <h2 className="text-secondary text-4xl font-extrabold">What our customers are sayings</h2>
+                <p className="mb-10 text-xl text-primary-content">Enhance posture, mobility, and well-being effortlessly with Posture Pro. Achieve proper alignment, reduce <br />  pain, and strengthen your body with ease!</p>
+            </div>
+
+            <Swiper
+                effect={'coverflow'}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={3}
+                spaceBetween={30}
+                loop={true}
+                autoplay={{
+                    delay: 2000,
+                    disableOnInteraction: false,
+                }}
+                coverflowEffect={{
+                    rotate: 70,
+                    stretch: "50%",
+                    depth: 200,
+                    scale: 0.75,
+                    modifier: 1,
+                    slideShadows: true,
+                }}
+                pagination={true}
+                modules={[EffectCoverflow, Pagination, Autoplay]}
+                className="mySwiper"
+            >
+                {
+                    reviews.map(review => <SwiperSlide key={review.id}>
+                        <ReviewCard review={review}></ReviewCard>
+                    </SwiperSlide>)
+                }
+
+            </Swiper>
+
+        </div>
+    );
+};
+
+export default Reviews;
