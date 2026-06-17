6c96f374022b7012d0c3cc432fde34664dd85900
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-06-17 22:28:04 +0600

    added parcel delete functionality with confirmation modal

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -1,13 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaMagnifyingGlass, FaTrashCan } from "react-icons/fa6";
+import { FiEdit } from "react-icons/fi";
+import Swal from "sweetalert2";
 
 const MyParcels = () => {
 
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
 
-    const { data: parcels = [] } = useQuery({
+    const { data: parcels = [], refetch } = useQuery({
         queryKey: ['myParcels', user?.email],
         queryFn: async () => {
             const res = await axiosSecure.get(`/parcels?email=${user.email}`);
@@ -17,9 +20,77 @@ const MyParcels = () => {
 
     })
 
+    const handleParcelDelete = id => {
+        console.log(id);
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/parcels/${id}`)
+                    .then(res => {
+                        //updating ui after delete
+                        refetch()
+                        if (res.data.deletedCount) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your parcel has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+
+        });
+    }
     return (
         <div>
-            <h2>All my parcels: {parcels.length}</h2>
+            <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 m-8 rounded-2xl">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>cost</th>
+                            <th>Parcel Status</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            parcels.map((parcel, index) =>
+                                <tr key={parcel._id}>
+                                    <th>{index + 1}</th>
+                                    <td>{parcel.parcelName}</td>
+                                    <td>{parcel.cost}</td>
+                                    <td>Blue</td>
+                                    <td>
+                                        <button className="btn btn-square hover:bg-primary">
+                                            <FaMagnifyingGlass></FaMagnifyingGlass>
+                                        </button>
+                                        <button className="btn btn-square hover:bg-primary mx-2">
+                                            <FiEdit></FiEdit>
+                                        </button>
+                                        <button
+                                            onClick={() => handleParcelDelete(parcel._id)}
+                                            className="btn btn-square hover:bg-primary">
+                                            <FaTrashCan></FaTrashCan>
+                                        </button>
+
+                                    </td>
+                                </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
