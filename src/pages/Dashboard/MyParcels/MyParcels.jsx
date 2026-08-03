766f01ab4dd897aa9fd215aa4511f23cfd24c381
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-08-03 13:24:18 +0600

    serach user using partial regex

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -77,6 +77,7 @@ const MyParcels = () => {
                             <th>Name</th>
                             <th>Cost</th>
                             <th>Payment</th>
+                            <th>Tracking Id</th>
                             <th>Delivery Status</th>
                             <th>Actions</th>
                         </tr>
@@ -106,7 +107,8 @@ const MyParcels = () => {
 
                                         }
                                     </td>
-                                    <td>Blue</td>
+                                    <td>{parcel.trackingId}</td>
+                                    <td>{parcel.deliveryStatus}</td>
                                     <td>
                                         <button className="btn btn-square hover:bg-primary">
                                             <FaMagnifyingGlass></FaMagnifyingGlass>
--- src/pages/Dashboard/UsersManagement/UsersManagement.jsx
+++ src/pages/Dashboard/UsersManagement/UsersManagement.jsx
@@ -2,13 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { FaUserShield, FaUserSlash } from "react-icons/fa";
 import Swal from "sweetalert2";
+import { useState } from "react";
 
 const UsersManagement = () => {
     const axiosSecure = useAxiosSecure();
+    const [serachText,setSearchText] = useState('');
     const { refetch, data: users = [] } = useQuery({
-        queryKey: ['users'],
+        queryKey: ['users',serachText],
         queryFn: async () => {
-            const res = await axiosSecure.get(`/users`);
+            const res = await axiosSecure.get(`/users?searchText=${serachText}`);
             console.log(res.data);
             return res.data
         }
@@ -19,7 +21,7 @@ const UsersManagement = () => {
         const roleInfo = { role: role };
         axiosSecure.patch(`/users/${user._id}/role`, roleInfo)
             .then(res => {
-        
+
                 if (res.data.modifiedCount) {
                     refetch()
                     const message =
@@ -49,6 +51,25 @@ const UsersManagement = () => {
     return (
         <div>
             <h2 className="text-4xl">Manage Users:{users.length} </h2>
+            <label className="input">
+                <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <g
+                        strokeLinejoin="round"
+                        strokeLinecap="round"
+                        strokeWidth="2.5"
+                        fill="none"
+                        stroke="currentColor"
+                    >
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <path d="m21 21-4.3-4.3"></path>
+                    </g>
+                </svg>
+                <input 
+                onChange={(e) => setSearchText(e.target.value)}
+                type="search" 
+                required 
+                placeholder="Search" />
+            </label>
             <div className="overflow-x-auto">
                 <table className="table">
                     {/* head */}
